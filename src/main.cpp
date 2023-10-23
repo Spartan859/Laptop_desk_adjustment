d10de35d5988ad46de28639e4ac658cad2a04f6e
Spartan859 <spartan20859@gmail.com>
2023-10-23 23:48:01 +0800

    feature: auto height adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 
 int trigPin = 36;    //Trig
 int echoPin = 35;    //Echo
+int pwmA=4,Ain1=6,Ain2=5;
+int stby=7;
 long duration, cm, inches;
+
  
 void setup() {
   //Serial Port begin
@@ -11,18 +14,41 @@ void setup() {
   Serial.println("Started!!!!!!!");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(Ain1,OUTPUT);
+  pinMode(Ain2,OUTPUT);
+  pinMode(stby,OUTPUT);
+  digitalWrite(stby,HIGH);
+  /*
+  digitalWrite(Ain1,LOW);
+  digitalWrite(Ain2,HIGH);
+  analogWrite(pwmA,250);*/
+}
+
+void set_move_mode(int tp){
+    analogWrite(pwmA,200);
+    if(tp==0){
+        digitalWrite(Ain1,LOW);
+        digitalWrite(Ain2,LOW);
+    }else if(tp==1){
+        digitalWrite(Ain1,LOW);
+        digitalWrite(Ain2,HIGH);
+    }else if(tp==2){
+        digitalWrite(Ain1,HIGH);
+        digitalWrite(Ain2,LOW);
+    }
 }
  
 void loop()
 {
- 
- 
+  
+  
+  
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(15);
   digitalWrite(trigPin, LOW);
  
   // Read the signal from the sensor: a HIGH pulse whose
@@ -32,13 +58,13 @@ void loop()
  
   // convert the time into a distance
   cm = (duration/2) / 29.1;
-  inches = (duration/2) / 74; 
-  
-  Serial.print(inches);
-  Serial.print("in, ");
   Serial.print(cm);
   Serial.print("cm");
   Serial.println();
-  
-  delay(1000);
+  if(cm<=5){
+    set_move_mode(2);
+  }else if(cm>=7&&cm<=20){
+    set_move_mode(1);
+  }else set_move_mode(0);
+  delay(200);
 }
